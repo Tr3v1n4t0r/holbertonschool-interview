a674c527f7189b69ba6394898c7e7d1e4605a019
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-01-07 22:30:55 -0800

    Update 0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,12 +9,13 @@ def minOperations(n):
     """Returns the number of prime factors of n"""
     if (n <= 1):
         return 0
+    num = n
     i = 2
     total = 0
-    while (i <= n // 2):
-        if (n % i == 0):
+    while (i <= num // 2):
+        if (num % i == 0):
             total += i
-            n //= i
+            num //= i
         else:
             i += 1
-    return total + n
+    return total + num
