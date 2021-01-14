e959444a7419569c83d26b7e7516282626a68eec
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-01-14 12:27:52 -0800

    spaced things out

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,6 +4,7 @@
 def canUnlockAll(boxes):
     unlocked = [False] * len(boxes)
     unlocked[0] = True
+    
     changed = True
 
     while (changed):
@@ -12,11 +13,14 @@ def canUnlockAll(boxes):
         for i in range(len(boxes)):
             if (not unlocked[i]):
                 continue
+
             for j in range(len(boxes[i])):
                 if (boxes[i][j] >= len(boxes)):
                     continue
+
                 key = boxes[i][j]
                 if (not unlocked[key]):
                     unlocked[key] = True
                     changed = True
+
     return all(unlocked)
