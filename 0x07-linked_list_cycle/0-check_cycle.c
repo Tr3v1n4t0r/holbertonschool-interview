596456664039c241227947a11200a00d555398d2
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-02-03 14:55:13 -0800

    t0

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if there is a cycle in a linked list
+ * 
+ * @list: Pointer to the first node on the linked list
+ * 
+ * Return: 0 if no cycle, else 1
+ **/
+int check_cycle(listint_t *list)
+{
+        listint_t *t, *h;
+        if (!list)
+                return (0);
+        
+        t = list;
+        h = list;
+
+        while (h)
+        {
+                if (h->next)
+                        h = h->next->next;
+                else
+                        return (0);
+
+                t = t->next;
+
+                if (h == t)
+                        return (1);
+        }
+
+        return (0);
+}
