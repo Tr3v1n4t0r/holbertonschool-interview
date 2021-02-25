0d887dd75faca478f25047c631ee54ec8ca8a481
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-02-25 01:58:15 -0800

    shrug

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -11,43 +11,43 @@
  **/
 int slide_line(int *line, size_t size, int direction)
 {
-        uint bound, on, move_to, moved = 0;
-        int slide;
+    uint bound, on, move_to, moved = 0;
+    int slide;
 
-        if (!line)
-                return (0);
-    
-        if (direction != SLIDE_LEFT && direction !- SLIDE_RIGHT)
-                return (0);
+    if (!line)
+        return (0);
 
-        slide = (direction ? -1 : 1);
-        bound = (direction ? size - 1 : 0);
+    if (direction != SLIDE_LEFT && direction !- SLIDE_RIGHT)
+            return (0);
 
-        for (on = bound; on < size; on += slide)
-        {
-                if (line[on] == 0)
-                        continue;
+    slide = (direction ? -1 : 1);
+    bound = (direction ? size - 1 : 0);
 
-                moved = 0;
+    for (on = bound; on < size; on += slide)
+    {
+            if (line[on] == 0)
+                    continue;
 
-                for (move_to = bound;
-                        ((direction == SLIDE_LEFT && move_to < on) ||
-                        (direction == SLIDE_RIGHT && move_to > on)) && !moved;
-                        move_to += slide)
-                {
-                        if (line[move_to] == 0 || line[move_to] == line[on])
-                        {
-                                line[move_to] += line[on];
-                                line[on] = 0;
-                                bound = move_to + (line[move_to] == line[on] ? slide : 0);
-                                moved = 1;
-                        }
-                }
+            moved = 0;
 
-                if (!moved)
-                        bound = on;
-        }
+            for (move_to = bound;
+                    ((direction == SLIDE_LEFT && move_to < on) ||
+                    (direction == SLIDE_RIGHT && move_to > on)) && !moved;
+                    move_to += slide)
+            {
+                    if (line[move_to] == 0 || line[move_to] == line[on])
+                    {
+                            line[move_to] += line[on];
+                            line[on] = 0;
+                            bound = move_to + (line[move_to] == line[on] ? slide : 0);
+                            moved = 1;
+                    }
+            }
 
-        return (1);
+            if (!moved)
+                    bound = on;
+    }
+
+    return (1);
 
 }
