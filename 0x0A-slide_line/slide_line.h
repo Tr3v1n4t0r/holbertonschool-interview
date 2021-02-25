9d43f945757097e58fd123a010efd0a2434f0adc
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-02-25 01:41:51 -0800

    t0

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,11 @@
+#ifndef  __SLIDE_LINE__
+#define __SLIDE_LINE__
+
+#include <stdlib.h>
+
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif __SLIDE_LINE__
