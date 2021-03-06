14a4023ef41d5077618b009fd5ce6dbcbef0fa73
Tr3v1n4t0r <813@holbertonschool.com>
2021-03-06 11:29:42 -0800

    I hate pep8

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -8,6 +8,7 @@ info = {"200": 0, "301": 0, "400": 0, "401": 0,
         "403": 0, "404": 0, "405": 0, "500": 0}\
 
 
+
 def print_info():
     print("File size: {}".format(sum))
     sc = sorted(info.keys())
